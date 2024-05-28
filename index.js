5fa592a9f24bf4f20b3c45102a7a435afee41a8e
Ramiro Rivera <ramiroriveraok@gmail.com>
2024-05-27 21:21:53 -0300

    js hecho

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-let edad = 20;
+const aniodeNacimiento = prompt('ingrese su ano de nacimiento:');
+let edad = 2024 - aniodeNacimiento;
 if (edad < 18){
-    console.log('Todavia no es mayor de edad');
-} else if (edad >= 18);
-console.log('Usted es mayor de edad')
+   option = prompt('Usted es menor de edad, ingrese una opcion:  1. Jugos  2. Bebidas Energeticas  0. salir');
+if (option == 1){
+    alert("Jugo $500");
+} else if (option == 2){
+    alert("Bebidas energeticas $100 c/u");
+}
+} else  if (edad >= 18)  {
+ option = prompt('Usted es mayor de edad, ingrese una opcion:  1. Jugos  2. Bebidas Energeticas  3. Vodka  4. Whisky  0. salir ');
+ if (option == 1){
+    alert("Jugo $500");
+} else if (option == 2){
+    alert("Bebidas energeticas $100 c/u");
+} else if( option == 3){
+    alert("Vodka $2000");
+} else if( option == 4){
+    alert("Whisky $7000")
+} else if (option == 0){
+    alert("Gracias por visitar nuestra paguina, Vuelva pronto!")
+} 
+}
